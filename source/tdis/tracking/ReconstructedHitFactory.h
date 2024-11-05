7decefa90f480dfe3f13c3fe7bb53957294916a3
Dmitry Romanov <romanovda@gmail.com>
2024-11-05 10:22:46 -0500

    Pre working updates

---
+++ source/tdis/tracking/ReconstructedHitFactory.h
@@ -0,0 +1,9 @@
+//
+// Created by romanov on 11/1/2024.
+// Subject to the terms in the LICENSE file found in the top-level directory.
+//
+
+#ifndef RECONSTRUCTEDHITFACTORY_H
+#define RECONSTRUCTEDHITFACTORY_H
+
+#endif //RECONSTRUCTEDHITFACTORY_H
